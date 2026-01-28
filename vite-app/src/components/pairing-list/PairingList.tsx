72d98a475128daac6e28e01b0a25868cb1a93524
Travis Adams <brokenCD@live.com>
2026-01-28 12:25:15 +0700

    create pairing list component

---
+++ vite-app/src/components/pairing-list/PairingList.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import type { Pairing } from "../../types";
+import "./PairingList.css";
+
+interface PairingListProps {
+  pairings: Pairing[];
+}
+
+const PairingList: React.FC<PairingListProps> = ({ pairings }) => {
+  return (
+    <div className="pairing-list">
+      {pairings.map((p) => (
+        <div key={p.id} className="pairing-card">
+          <div className="image-container">
+            <img src={p.image} alt={p.wine} className="pairing-image" />
+          </div>
+          <div className="card-content">
+            <h3>{p.wine}</h3>
+            <p>{p.food}</p>
+            <p>{p.region}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default PairingList;
